f4bfea1a42573639c649ab47d13761a253ea301a
Osman Çelik <76453193+B3B3K@users.noreply.github.com>
2022-08-14 10:46:04 +0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,16 @@
+apt-get update && sudo apt-get upgrade
+apt-get install wget
+apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
+apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y 
+apt-get install libxvidcore-dev libx264-dev -y
+apt-get install libgtk2.0-dev -y
+apt-get install libatlas-base-dev gfortran -y
+apt-get install python3-pip -y
+pip install numpy 
+apt list python*opencv*
+apt install python3-opencv
+pip install opencv-contrib-python 
+apt show python3-opencv
+clear
+echo export: import cv2
+python3
